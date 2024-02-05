f00934dfac66682e35713cb0f8d8336d5c827039
Torremolinos <adrian_2054@hotmail.com>
2024-02-05 11:51:52 +0100

    configurando todo y empezando con el header.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/*.html' // add your html file here 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
